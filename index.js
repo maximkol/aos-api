a382b6ae5f6b0a096e065d812701e89cdea0fc0e
Maxim Kol <maxim.kol@gdc-services.com>
2023-01-24 21:27:00 +0000

    get data from db

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-const tournaments = require('./data.js');
+//const tournaments = require('./data.js');
+const mysql = require('mysql2/promise');
 const express = require('express');
 const app = express();
 const port = 3000;
+const config = {
+    db: {
+      /* don't expose password or any sensitive info, done only for demo */
+      host: "sapfir103.beget.tech",
+      user: "",
+      password: "",
+      database: "",
+    },
+    listPerPage: 10,
+  };
 
+async function query(sql) {
+    const connection = await mysql.createConnection(config.db);
+    const [results, ] = await connection.execute(sql);
+
+    return results;
+}
+let rows = [];
+query(`SELECT * FROM tournaments`)
+.then(data=>{
+    rows = data;
+});
+   
 app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/api/tournaments', (req, res)=>{
-    console.log(tournaments);
-    res.json(tournaments);
+    console.log(rows);
+    res.json(rows);
 })
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
